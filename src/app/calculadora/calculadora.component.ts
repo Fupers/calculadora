cc56ac7e592bda2401a871e60aaac3770056dcb6
Fupers <guevara.andres20@gmail.com>
2024-07-22 04:46:59 -0400

    commit1: modificacion en la estructura de la pagina y de la calculadora en css y html

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+
+}
